424bee9f6622743e1a0ebca740eb83bf5b5b0fc6
najihun <jihun.na@hashicorp.com>
2023-10-22 00:28:09 +0900

    linux ip custom

--- provider.tf
+++ provider.tf
@@ -4,9 +4,18 @@ terraform {
       source = "hashicorp/aws"
       version = "5.22.0"
     }
+
+    template = {
+      source = "hashicorp/template"
+      version = "2.2.0"
+    }
   }
 }
 
 provider "aws" {
     region = var.region 
+}
+
+provider "template" {
+  # Configuration options
 }
